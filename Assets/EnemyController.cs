a162752f13f154b5a3a1c838c0bd2d3e516deb24
vacolavita <vacolavita@gmail.com>
2023-04-18 11:53:30 -0400

    AI

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -13,27 +13,97 @@ public class EnemyController : MonoBehaviour
 
     private int index = 0;
 
+    private bool chasing = false;
+
+    public GameObject player;
+
+    private SphereCollider col;
+
+    public float FOV = 90;
+
+    private Vector3 pos;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        col = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer <= 0)
+        if (!chasing)
         {
-            agent.SetDestination(pathPoints[index]);
-            index++;
-            timer = waitTime;
-            if(index > pathPoints.Length - 1)
+            if (timer <= 0)
             {
-                index = 0;
+                agent.SetDestination(pathPoints[index]);
+                index++;
+                timer = waitTime;
+                if (index > pathPoints.Length - 1)
+                {
+                    index = 0;
+                }
+            }
+
+            if (agent.remainingDistance != Mathf.Infinity && agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance == 0)
+            {
+                timer -= Time.deltaTime;
             }
         }
-        if (agent.remainingDistance != Mathf.Infinity && agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance == 0) {
-            timer -= Time.deltaTime;
+        else
+        {
+            pos = player.transform.position;
+            agent.SetDestination(pos);
+
+            Vector3 direction = player.transform.position - transform.position;
+            float angle = Vector3.Angle(direction, transform.forward);
+            RaycastHit hit;
+            Debug.DrawLine(transform.position + transform.up * 0.1f, player.transform.position, Color.red, 0);
+            if (Physics.Raycast(transform.position + transform.up * 0.1f, direction.normalized, out hit, col.radius))
+            {
+
+                if (hit.collider.gameObject != player || Vector3.Distance(transform.position, player.transform.position) > col.radius * 1.2)
+                {
+                    timer -= Time.deltaTime;
+                    if (timer <= 0)
+                    {
+                        chasing = false;
+                        timer = 3f;
+                        agent.SetDestination(gameObject.transform.position);
+                    }
+                }
+                else
+                {
+                    timer = 1f;
+                }
+            }
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            Vector3 direction = other.transform.position - transform.position;
+            float angle = Vector3.Angle(direction, transform.forward);
+            RaycastHit hit;
+            if (!chasing)
+            {
+                Debug.DrawLine(transform.position + transform.up * 0.1f, player.transform.position, Color.green, 0);
+            }
+            if (angle < FOV * 0.5)
+            {
+                if (Physics.Raycast(transform.position + transform.up * 0.1f, direction.normalized, out hit, col.radius))
+                {
+                    if (hit.collider.gameObject == player)
+                    {
+                        chasing = true;
+                        timer = 1f;
+                    }
+                }
+            }
         }
     }
 }
